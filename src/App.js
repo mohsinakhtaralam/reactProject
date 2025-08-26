ed283901f26ef8f21c4857d2074cbd68bfbaf0bd
Mohsin Akhtar Alam <mdxb.info@gmail.com>
2025-08-26 16:24:18 +0500

    uploaded from somewhere else

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Never try to Learn React and GIT in your life
+          Always try to Learn React and GIT in your life with some one
         </a>
       </header>
     </div>
